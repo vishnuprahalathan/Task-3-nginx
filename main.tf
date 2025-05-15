51d218ee7e56355d97d395516fef89709bdcee2b
vishnuprahalathan <vishnuprahalathan1@gmail.com>
2025-05-15 12:20:20 +0530

    Initial commit for Terraform Task 3 - Nginx

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx_image" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx-server"
+  image = docker_image.nginx_image.name
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
